3897d77a85ace9d9dfbb4f9f0db44fbbe71e0e01
iamAbayomi <oladinifolawiyo@gmail.com>
2023-04-30 08:26:34 +0100

    refactor: removed unused code

--- 5.HigherOrderFunctions/exercises/flattening/index.js
+++ 5.HigherOrderFunctions/exercises/flattening/index.js
@@ -1,21 +1,15 @@
 // Use the reduce method in combination with the concat method to "flatten"
-// an array of arrays into a single array that has all the elements of the original 
-// arrays.
+// an array of arrays into a single array that has all the elements of the original  arrays.
 
 function flattening(arrayofArrays) {
-    //  console.log("arrays of arrays ", arrayofArrays)
-    let tempArrays = []
     return arrayofArrays.reduce((a, b) => a.concat(b), [])
-    return arrayofArrays
+}
+
+function logValue(value) {
+    console.log(value)
 }
 
 let arrayofArrays = [[1, 2], [3, 4], [5, 6], [7, 8]]
 let otherArrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-//flattening(arrayofArrays)
-
 logValue(flattening(arrayofArrays))
-
-function logValue(value) {
-    console.log("value ", value)
-}
